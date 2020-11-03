a3a1fcb3c65ab6be40d757e07b73256e2fb8e746
lhuangon <lhuan.95@gmail.com>
2020-11-02 22:20:04 -0300

    estudos

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 61
+
+console.log(`A velocidade do seu carro ${vel}Km/h`)
+
+if(vel > 60) { //condição simples
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança!`)
